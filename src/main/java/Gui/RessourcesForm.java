37c25b0213500d24f7aa3deb5ebde7e3055f75c9
Unknown <lloysekrug@gmail.com>
2018-05-04 15:41:53 +0200

    Ressources not finished

--- src/main/java/Gui/RessourcesForm.java
+++ src/main/java/Gui/RessourcesForm.java
@@ -3,23 +3,50 @@ package Gui;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.LinkedList;
+
 import Client.*;
 
 public class RessourcesForm {
 
     private Client client;
-    private JPanel panel1;
+    private JPanel mainPanel;
     private JButton deconnecterButton;
     private JButton actualiserButton;
-    private JList list1;
-    private JList list2;
+    private JPanel RessourcesPanel;
+    private JPanel ObjectsPanel;
+    private JPanel ActionPanel;
+    private LinkedList<JLabel> ressources = new LinkedList<JLabel>();
+    private LinkedList<JLabel> objects = new LinkedList<JLabel>();
 
-    public RessourcesForm() {
+
+    public RessourcesForm(Client client) {
+        this.client = client;
+        updateRessources();
+
         actualiserButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-
+                updateRessources();
             }
         });
+        deconnecterButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //client.disconnect()
+            }
+        });
+    }
+
+    private void updateRessources(){
+
+        //ressources = client.getAllClientRessrouces;
+        for(JLabel r : ressources){
+            RessourcesPanel.add(r);
+        }
+
+        //objects = client.getAllClientObjects;
+        for(JLabel o : objects){
+            ObjectsPanel.add(o);
+        }
     }
 
     private void createUIComponents() {
