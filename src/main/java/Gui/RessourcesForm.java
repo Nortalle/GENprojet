389be0e5c754a3f80388be6de974ee31bc8ae54d
Unknown <lloysekrug@gmail.com>
2018-05-04 15:01:01 +0200

    Debut ressource gui

---
+++ src/main/java/Gui/RessourcesForm.java
@@ -0,0 +1,28 @@
+package Gui;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import Client.*;
+
+public class RessourcesForm {
+
+    private Client client;
+    private JPanel panel1;
+    private JButton deconnecterButton;
+    private JButton actualiserButton;
+    private JList list1;
+    private JList list2;
+
+    public RessourcesForm() {
+        actualiserButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
