d3bbee9c6cccc98f7251216a2150e4fd3b824378
Nortalle <vincent.guidoux1@heig-vd.ch>
2018-05-17 12:25:16 +0200

    LEs ressources apparaissent dans le menu

--- src/main/java/Gui/ClientForm.java
+++ src/main/java/Gui/ClientForm.java
@@ -2,10 +2,20 @@ package Gui;
 
 import javax.swing.*;
 import Client.*;
+import Game.Resources;
+
+import java.awt.*;
+import java.awt.event.FocusAdapter;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 public class ClientForm {
 
-    private JPanel ClientFromMainFrame;
+    private Logger LOG = Logger.getInstance();
+
+    private JPanel panel_main;
 
     //Ressources
     private JPanel RessouresGUI;
@@ -21,17 +31,45 @@ public class ClientForm {
 
     //Logs
     private JPanel Logs;
-    private JTextArea LoggerTextArea;
+    private JTextPane LoggerTestPane;
+    private JPanel RessourcesPanel;
+    private JLabel eau_i;
+    private JLabel bois_i;
+    private JLabel coal_i;
+    private JLabel oil_i;
+    private JLabel iron_ore_i;
+    private JLabel copper_ore_i;
+    private JLabel steel_i;
+    private JLabel gold_ore_i;
+    private JLabel scrum_i;
 
     private Client client;
 
     public ClientForm(Client client) {
         this.client = client;
+        updateResources();
 
-        //RessouresGUI.setUI();
+        LOG.log("Ouverture de ce que le client va voir");
 
-        RessouresGUI.setUI(new RessourcesForm(client).getPanel_main().getUI());
 
-        LoggerTextArea.append(Logger.getInstance().getLog());
+    }
+
+    private void updateResources(){
+        String answer = client.getResources();
+        Resources resources = new Resources(answer);
+        scrum_i.setText(Integer.toString(resources.getScrum()));
+        eau_i.setText(Integer.toString(resources.getEau()));
+        bois_i.setText(Integer.toString(resources.getBois()));
+        coal_i.setText(Integer.toString(resources.getCharbon()));
+        oil_i.setText(Integer.toString(resources.getPetrol()));
+        iron_ore_i.setText(Integer.toString(resources.getFer()));
+        copper_ore_i.setText(Integer.toString(resources.getCuivre()));
+        steel_i.setText(Integer.toString(resources.getAcier()));
+        gold_ore_i.setText(Integer.toString(resources.getOr()));
+
+    }
+
+    public JPanel getPanel_main() {
+        return panel_main;
     }
 }
