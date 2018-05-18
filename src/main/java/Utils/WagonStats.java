7e228dda1f630b08df62bb2fcedd476c22d0d356
kam_ <kamil.amrani@heig-vd.ch>
2018-05-18 13:24:31 +0200

    define WagonStats

---
+++ src/main/java/Utils/WagonStats.java
@@ -0,0 +1,68 @@
+package Utils;
+
+public class WagonStats {
+
+    // --- LOCO --- //
+    public static final int LOCO_ID = 1;
+    public static final String LOCO_NAME = "Loco";
+
+    // --- DRILL WAGON --- //
+    public static final int DRILL_ID = 2;
+    public static final String DRILL_NAME = "Drill wagon";
+    public static final String[] DRILL_CAN_MINE = {"Charbon", "Fer", "Cuivre", "Acier", "Or"};// change to ID ?
+    // -- level 1 -- //
+    public static final int DRILL_V1_MINING_TIME = 10;
+    public static final int DRILL_V1_MINING_AMOUNT = 3;
+    // -- level 2 -- //
+    public static final int DRILL_V2_MINING_TIME = 8;
+    public static final int DRILL_V2_MINING_AMOUNT = 3;
+    // -- level 3 -- //
+    public static final int DRILL_V3_MINING_TIME = 6;
+    public static final int DRILL_V3_MINING_AMOUNT = 3;
+    // -- level 4 -- //
+    public static final int DRILL_V4_MINING_TIME = 6;
+    public static final int DRILL_V4_MINING_AMOUNT = 6;
+    // -- level 5 -- //
+    public static final int DRILL_V5_MINING_TIME = 5;
+    public static final int DRILL_V5_MINING_AMOUNT = 6;
+
+    // --- SAW WAGON --- //
+    public static final int SAW_ID = 3;
+    public static final String SAW_NAME = "Saw wagon";
+    public static final String[] SAW_CAN_MINE = {"Wood"};// change to ID ?
+    // -- level 1 -- //
+    public static final int SAW_V1_MINING_TIME = 5;
+    public static final int SAW_V1_MINING_AMOUNT = 10;
+    // -- level 2 -- //
+    public static final int SAW_V2_MINING_TIME = 4;
+    public static final int SAW_V2_MINING_AMOUNT = 10;
+    // -- level 3 -- //
+    public static final int SAW_V3_MINING_TIME = 4;
+    public static final int SAW_V3_MINING_AMOUNT = 15;
+    // -- level 4 -- //
+    public static final int SAW_V4_MINING_TIME = 3;
+    public static final int SAW_V4_MINING_AMOUNT = 20;
+    // -- level 5 -- //
+    public static final int SAW_V5_MINING_TIME = 3;
+    public static final int SAW_V5_MINING_AMOUNT = 25;
+
+    // --- PUMP WAGON --- //
+    public static final int PUMP_ID = 4;
+    public static final String PUMP_NAME = "Pump wagon";
+    public static final String[] PUMP_CAN_MINE = {"Eau", "Pertrol"};// change to ID ?
+    // -- level 1 -- //
+    public static final int PUMP_V1_MINING_TIME = 20;
+    public static final int PUMP_V1_MINING_AMOUNT = 1;
+    // -- level 2 -- //
+    public static final int PUMP_V2_MINING_TIME = 18;
+    public static final int PUMP_V2_MINING_AMOUNT = 1;
+    // -- level 3 -- //
+    public static final int PUMP_V3_MINING_TIME = 14;
+    public static final int PUMP_V3_MINING_AMOUNT = 1;
+    // -- level 4 -- //
+    public static final int PUMP_V4_MINING_TIME = 10;
+    public static final int PUMP_V4_MINING_AMOUNT = 2;
+    // -- level 5 -- //
+    public static final int PUMP_V5_MINING_TIME = 10;
+    public static final int PUMP_V5_MINING_AMOUNT = 3;
+}
