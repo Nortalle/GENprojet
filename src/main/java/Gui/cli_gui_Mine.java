ea5ba11a1f2a3e5ccacb3e04705073f29ce1b070
kam_ <kamil.amrani@heig-vd.ch>
2018-05-24 12:44:17 +0200

    min gui bug

--- src/main/java/Gui/cli_gui_Mine.java
+++ src/main/java/Gui/cli_gui_Mine.java
@@ -5,4 +5,8 @@ import javax.swing.*;
 public class cli_gui_Mine {
     private JComboBox comboBox1;
     private JButton startMiningButton;
+
+    public void update(){
+        startMiningButton.setText("test button start mining");
+    }
 }
