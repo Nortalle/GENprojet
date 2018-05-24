aa6fad17943f61e9a0d5badc5801b0adaaec6822
kam_ <kamil.amrani@heig-vd.ch>
2018-05-24 13:38:07 +0200

    fix gui mine

--- src/main/java/Gui/cli_gui_Mine.java
+++ src/main/java/Gui/cli_gui_Mine.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 public class cli_gui_Mine {
     private JComboBox comboBox1;
     private JButton startMiningButton;
+    private JPanel panel1;
 
     public void update(){
         startMiningButton.setText("test button start mining");
