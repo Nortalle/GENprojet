242e078481da086d8a58b420b1a521926220d56a
kam_ <kamil.amrani@heig-vd.ch>
2018-05-25 10:29:31 +0200

    clear mining test

--- src/test/java/Server/Controller/MineControllerTest.java
+++ src/test/java/Server/Controller/MineControllerTest.java
@@ -14,20 +14,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MineControllerTest {
-    private static MineController mc;
-
-    private static Mine mine1 = new Mine(1, 1, 30, 2);
-    private static Mine mine2 = new Mine(2, 2, 1000, 2);
-    private static Mine mine3 = new Mine(3, 3, 900, 2);
 
     @BeforeAll
     public static void setUpBeforeAll() {
-        mc = new MineController();
-        mine1 = new Mine(1, 1, 30, 2);
-        mine2 = new Mine(2, 2, 1000, 2);
-        mine3 = new Mine(3, 3, 900, 2);
-
-
     }
 
     @BeforeEach
@@ -36,20 +25,6 @@ class MineControllerTest {
 
     @Test
     public void testMinage(){
-
-        Wagon wagon = new Wagon(1 , 1000, 1, 2);
-        WagonMining wagonMining = new WagonMining(wagon, mine1);
-
-        mc.addWagon(wagonMining);
-
-        long start = System.currentTimeMillis();
-        while(System.currentTimeMillis() - start < 1200){
-
-        }
-
-        assertEquals(450 - WagonStats.getMiningTime(2,1), mine1.getAmount());
-
-
     }
 
 
