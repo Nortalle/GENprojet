7768b435180d5dd4b75322859c77e461111bf9a9
kamkill01011 <kamil.amrani@heig-vd.ch>
2018-06-02 18:13:13 +0200

    fix db delete == 0 -> == 1

--- src/main/java/Server/DataBase.java
+++ src/main/java/Server/DataBase.java
@@ -120,7 +120,7 @@ public class DataBase {
             PreparedStatement ps = connection.prepareStatement("DELETE FROM Utilisateur WHERE nomUtilisateur=?;", Statement.RETURN_GENERATED_KEYS);
             ps.setObject(1, username);
             int status = ps.executeUpdate();
-            if(status == 0) return true;
+            if(status == 1) return true;
             else return false;
         } catch (SQLException e) {
             e.printStackTrace();
