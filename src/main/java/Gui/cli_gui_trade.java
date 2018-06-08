b11f57b96ecda1c7f5bad13216e7a600f1901915
Perceval <dnamella1@hotmail.com>
2018-06-08 11:56:42 +0200

    trading gui

---
+++ src/main/java/Gui/cli_gui_trade.java
@@ -0,0 +1,13 @@
+package Gui;
+
+import javax.swing.*;
+
+public class cli_gui_trade {
+    private JComboBox dropdown_ressource_offer;
+    private JComboBox dropdown_ressource_for;
+    private JTextField quantity_offer;
+    private JTextField quantity_for;
+    private JButton searchButton;
+    private JButton placeButton;
+    private JPanel trading_panel;
+}
